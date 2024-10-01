27536aa703d0cdc46867f64fe29ae94f18dc11d0
Ben Nguyen <nben19733@gmail.com>
2024-10-01 14:25:51 +0700

    edit left sidebar

---
+++ resources/views/components/left-sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar-widget widget_categories_2 border-radius-10 bg-white mb-30">
+    <ul class="font-small text-muted">
+{{--        <li class="cat-item cat-item-2 active"><a href="#"><span class="mr-10">--}}
+{{--                                            <ion-icon name="earth-outline"></ion-icon>--}}
+{{--                                        </span>Giá vàng</a></li>--}}
+        <li class="cat-item cat-item-3"><a href="#"><span class="mr-10">
+                                            <ion-icon name="trending-up-outline"></ion-icon>
+                                        </span>Hàng hoá</a></li>
+        <li class="cat-item cat-item-5"><a href="#"><span class="mr-10">
+                                            <ion-icon name="bar-chart-outline"></ion-icon>
+                                        </span>Chứng khoán</a></li>
+{{--        <li class="cat-item cat-item-6"><a href="#"><span class="mr-10">--}}
+{{--                                            <ion-icon name="cash-outline"></ion-icon>--}}
+{{--                                        </span>Ngoại hối</a></li>--}}
+        <li class="cat-item cat-item-2"><a href="#"><span class="mr-10">
+                                            <ion-icon name="logo-bitcoin"></ion-icon>
+                                        </span>Crypto</a></li>
+    </ul>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,60 +6,10 @@
             <!-- sidebar-left -->
             <div class="col-lg-2 col-md-3 primary-sidebar sticky-sidebar sidebar-left order-2 order-md-1">
                 <!-- Widget Weather -->
-                <div class="sidebar-widget widget-weather border-radius-10 bg-white mb-30 mt-55">
-                    <div class="d-flex">
-                        <div class="font-medium">
-                            <p>Friday</p>
-                            <h2>27</h2>
-                            <p><strong>Sep</strong></p>
-                        </div>
-                        <div class="font-medium ml-10 pt-20">
-                            <div id="datetime" class="d-inline-block">
-                                <ul>
-                                    <li><span class="font-small">
-                                                    <a class="text-primary" href="#">London</a><br>
-                                                    <i class="wi wi-day-sunny mr-5"></i>32ºc
-                                                </span>
-                                        <p>Sunny</p>
-                                    </li>
-                                    <li><span class="font-small">
-                                                    <a class="text-danger" href="#">Paris</a><br>
-                                                    <i class="wi wi-day-cloudy mr-5"></i>28ºc
-                                                </span>
-                                        <p>Cloudy</p>
-                                    </li>
-                                    <li><span class="font-small">
-                                                    <a class="text-success" href="#">New York</a><br>
-                                                    <i class="wi wi-rain-mix mr-5"></i>25ºc
-                                                </span>
-                                        <p>Rainy</p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Widget Categories -->
-                <div class="sidebar-widget widget_categories_2 border-radius-10 bg-white mb-30">
-                    <ul class="font-small text-muted">
-                        <li class="cat-item cat-item-2 active"><a href="#"><span class="mr-10">
-                                            <ion-icon name="earth-outline"></ion-icon>
-                                        </span>Giá vàng</a></li>
-                        <li class="cat-item cat-item-3"><a href="#"><span class="mr-10">
-                                            <ion-icon name="trending-up-outline"></ion-icon>
-                                        </span>Giá dầu</a></li>
-                        <li class="cat-item cat-item-5"><a href="#"><span class="mr-10">
-                                            <ion-icon name="bar-chart-outline"></ion-icon>
-                                        </span>Hàng hoá</a></li>
-                        <li class="cat-item cat-item-6"><a href="#"><span class="mr-10">
-                                            <ion-icon name="cash-outline"></ion-icon>
-                                        </span>Ngoại hối</a></li>
-                        <li class="cat-item cat-item-2"><a href="#"><span class="mr-10">
-                                            <ion-icon name="logo-bitcoin"></ion-icon>
-                                        </span>Tiền điện tử</a></li>
-                    </ul>
-                </div>
-                <!-- Widget Categories -->
+                <x-widget-weather />
+                <!-- Left Sidebar Categories -->
+                <x-left-sidebar />
+                <!-- Left Sidebar Categories -->
 
             </div>
             <!-- main content -->
